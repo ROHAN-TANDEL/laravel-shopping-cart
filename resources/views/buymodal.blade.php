0c51f43edffdb23ba91a3d43529b4e3d304f8365
rohan <rohan.t@scriptbees.com>
2018-11-20 17:37:56 +0530

    testing payment for payU and paypal and braintreeSDk

--- resources/views/buymodal.blade.php
+++ resources/views/buymodal.blade.php
@@ -15,60 +15,15 @@
           <img class="img-fluid img-responsive rounded slide-photo" src="{{$data['mainTitle']}}" />
       <br>
       <h5>{{$data['mainContent']}}</h5>
-      <passport-clients></passport-clients>
-      <passport-authorized-clients></passport-authorized-clients>
-      <passport-personal-access-tokens></passport-personal-access-tokens>
-
-         <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                @if ($message = Session::get('success'))
-                <div class="custom-alerts alert alert-success fade in">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                    {!! $message !!}
-                </div>
-                <?php Session::forget('success');?>
-                @endif
-
-                @if ($message = Session::get('error'))
-                <div class="custom-alerts alert alert-danger fade in">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                    {!! $message !!}
-                </div>
-                <?php Session::forget('error');?>
-                @endif
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" id="payment-form" role="form" action="{!! URL::route('paypal') !!}" >
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('amount') ? 'has-error' : '' }}">
-                            <label for="amount" class="col-md-4 control-label">Amount</label>
-
-                            <div class="col-md-6">
-                                <input id="amount" type="text" class="form-control" name="amount" value="{{ old('amount') }}" autofocus>
-
-                                @if ($errors->has('amount'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('amount') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Paywith Paypal
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+          <form class="container text-center col-md-4 " method="POST" id="payment-form"  action="/payment/add-funds/paypal">
+            {{ csrf_field() }}
+            <h2 class="w3-text-blue">Payment Form</h2>
+            <p>Pay Money</p>
+            <p>      
+            <label class="w3-text-blue"><b>Enter Amount</b></label>
+            <input class="w3-input w3-border" name="amount" type="text"></p>      
+            <button class="w3-btn w3-blue">Pay with PayPal</button></p>
+          </form>
         </div>
         
         <!-- Modal footer -->
