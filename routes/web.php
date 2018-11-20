c0d06617b43b5b69b2e9fc98f1fa8979f4e81453
rohan <rohan.t@scriptbees.com>
2018-11-20 17:39:08 +0530

    routes update for social login

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () { return view('welcome'); });
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
